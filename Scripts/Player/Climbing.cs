8c0b078d68f0ec7172d092da4cd92f4ad2bd4e04
Vadim Niedental <vadim.niedental@gmail.com>
2024-12-17 00:20:09 +0100

    Climbing now works as intended. Future plans on adding a "Climbdash" to propel upwards.

--- Scripts/Player/Climbing.cs
+++ Scripts/Player/Climbing.cs
@@ -3,19 +3,23 @@ using Godot;
 public partial class Climbing : Resource
 {
     private Player Player;
+    private PackedScene _abilityPopupScene;
+
+    private bool climbUnlocked = false;
 
     public void SetPlayer(Player player)
     {
         Player = player;
+        _abilityPopupScene = GD.Load<PackedScene>("res://Scenes/AbilityPopup.tscn");
     }
-
 
     public void HandleClimb()
     {
-        if (Player.canClimb)
+        if (Player.canClimb && climbUnlocked)
         {
-            if (Input.IsActionPressed("shoot") && Player._wallRaycastLeft.IsColliding() && !Player.IsOnFloor() || Input.IsActionPressed("shoot") && Player._wallRaycastRight.IsColliding() && !Player.IsOnFloor())
+            if (Input.IsActionPressed("hold") && Player._wallRaycastLeft.IsColliding() && !Player.IsOnFloor() || Input.IsActionPressed("hold") && Player._wallRaycastRight.IsColliding() && !Player.IsOnFloor())
             {
+                GD.Print("Climbing");
                 Player.isClimbing = true;
                 Player.velocity = new Vector2(0, 0);
                 Player._sprite.Play("climb");
@@ -39,4 +43,21 @@ public partial class Climbing : Resource
         else
             Player.isClimbing = false;
     }
+
+    public void UnlockClimb()
+    {
+        climbUnlocked = true;
+        UnlockMessage();
+    }
+
+    private void UnlockMessage()
+    {
+        var abilityPopup = _abilityPopupScene.Instantiate<AbilityPopup>();
+        Player.AddChild(abilityPopup);
+        abilityPopup.ShowAbilityPopup(
+            "Wall Climbing", 
+            "You can now climb walls by holding the 'hold' button next to a wall. Use up and down to move vertically while climbing."
+        );
+    }
+    
 }
