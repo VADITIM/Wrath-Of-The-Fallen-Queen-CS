a50171949f8203a192c304190ac52b919dcc381b
Vadim Niedental <vadim.niedental@gmail.com>
2024-12-17 00:18:53 +0100

    Testing tweening.

--- Scripts/Misc/Coin.cs
+++ Scripts/Misc/Coin.cs
@@ -2,33 +2,46 @@ using Godot;
 
 public partial class Coin : Area2D
 {
-	public Sprite2D _sprite;
+    public Sprite2D _sprite;
 
-	private float coinCount = 0;
+    private float coinCount = 0;
 
-	public override void _Ready()
-	{
-		_sprite = GetNode<Sprite2D>("Sprite2D");
-	}
+    public override void _Ready()
+    {
+        _sprite = GetNode<Sprite2D>("Sprite2D");
 
-	private void OnBodyEntered(Player player)
-	{
-		if (player is Player)
-		{
-			CollectCoin();
-		}
-	}
+        Float();
+    }
 
-	private void CollectCoin()
-	{
-		QueueFree();
-		CoinIncrement();
-		GD.Print("Coin collected!");
-	}
+    private void Float()
+    {
+        Vector2 startPosition = Position;
+        Vector2 endPosition = Position + new Vector2(0, -10); 
 
-	private void CoinIncrement()
-	{
-		coinCount++;
-		GD.Print("Coin count: " + coinCount);
-	}
+        var tween = GetTree().CreateTween();
+        tween.TweenProperty(this, "position", endPosition, 1.0f).SetTrans(Tween.TransitionType.Sine).SetEase(Tween.EaseType.InOut);
+        tween.TweenProperty(this, "position", startPosition, 1.0f).SetTrans(Tween.TransitionType.Sine).SetEase(Tween.EaseType.InOut);
+        tween.SetLoops();
+    }
+
+    private void OnBodyEntered(Player player)
+    {
+        if (player is Player)
+        {
+            CollectCoin();
+        }
+    }
+
+    private void CollectCoin()
+    {
+        QueueFree();
+        CoinIncrement();
+        GD.Print("Coin collected!");
+    }
+
+    private void CoinIncrement()
+    {
+        coinCount++;
+        GD.Print("Coin count: " + coinCount);
+    }
 }
