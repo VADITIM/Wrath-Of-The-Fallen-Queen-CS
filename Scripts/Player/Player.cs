c99601fafd2d9e11c6ac07ddd6749a49268137dd
Vadim Niedental <vadim.niedental@gmail.com>
2024-12-16 01:03:58 +0100

    Player Script. Inherits every method to keep the script simple, expandable and readable.

---
+++ Scripts/Player/Player.cs
@@ -0,0 +1,137 @@
+using System;
+using Godot;
+
+public partial class Player : CharacterBody2D
+{
+    // NODES    --------------------------------------------------------------------------------------------------------
+    public AnimatedSprite2D _sprite;
+    public RayCast2D _wallRaycastLeft;
+    public RayCast2D _wallRaycastRight;
+    public TileMapLayer _damageLayer;
+    private Timer _iFrames;
+    public Timer _dashTimer;
+    public Sprite2D _dashIndicator;
+    // ----------------------------------------------------
+
+    // SCRIPTS  --------------------------------------------------------------------------------------------------------
+    private Movement Movement = new Movement();
+    private Climbing Climbing = new Climbing();
+    private Gravity Gravity = new Gravity();
+    private Jump Jump = new Jump();
+    public Dash Dash = new Dash();
+    private Coin Coin = new Coin();
+    private DashUnlocker DashUnlocker = new DashUnlocker();
+    // ----------------------------------------------------
+
+    [Export] public float speed = 600f;
+    [Export] public float jumpForce = -800f;
+    [Export] public float gravity = 30f;
+    [Export] public float dashSpeed = 900f;
+    [Export] public float climbSpeed = 100f;
+    [Export] public int health = 3;
+    private bool damageTaken = false;
+
+
+
+    public Vector2 velocity = Vector2.Zero;
+
+    public bool canClimb = true;
+    public bool isClimbing = false;
+    
+
+    public override void _Ready()
+    {
+        // NODES    --------------------------------------------------------------------------------------------------------
+        _sprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+        // var raycastsNode = GetNode<Node>("Raycasts");
+        _wallRaycastLeft = GetNode<RayCast2D>("WallRaycastLeft");
+        _wallRaycastRight = GetNode<RayCast2D>("WallRaycastRight");
+        _damageLayer = GetNode<TileMapLayer>("/root/World/DamageLayer");
+        _iFrames = GetNode<Timer>("IFrames");
+        _dashTimer = GetNode<Timer>("DashTimer");
+        _dashIndicator = GetNode<Sprite2D>("DashIndicator");
+        _dashIndicator.Visible = false;
+        // ----------------------------------------------------
+
+        // SCRIPTS  --------------------------------------------------------------------------------------------------------
+        Movement.SetPlayer(this);
+        Climbing.SetPlayer(this);
+        Gravity.SetPlayer(this);
+        Jump.SetPlayer(this);
+        Dash.SetPlayer(this);
+        // ----------------------------------------------------
+    }
+
+    public override void _PhysicsProcess(double delta)
+    {
+        Movement.HandleMovement();
+        Jump.HandleJump();
+
+        Gravity.HandleGravity();
+        Climbing.HandleClimb();
+
+        Dash.HandleDash();
+        FlipSprite();
+        CheckIfOnFloor();
+        CheckForDamage();
+
+        MoveAndSlide();
+    }
+
+    public void CollectUnlocker(Node collectedObject)
+    {
+        collectedObject.QueueFree();
+    }
+
+    private void CheckIfOnFloor()
+    {
+        if (IsOnFloor())
+        {
+            canClimb = true;
+        }
+    }
+
+    private void ApplyDamage()
+    {
+        if (damageTaken) return;
+
+        health--;
+        damageTaken = true;
+        _iFrames.Start();
+        GD.Print("Player health: " + health);
+    }
+    
+    private void CheckForDamage()
+    {
+        Vector2I tilePosition = _damageLayer.LocalToMap(GlobalPosition);
+
+        int sourceId = _damageLayer.GetCellSourceId(tilePosition);
+        if (sourceId != -1)
+        {
+            ApplyDamage();
+        }
+    }
+
+    private void FlipSprite()
+    {
+        if (Input.IsActionPressed("left"))
+            _sprite.FlipH = true;
+        else if (Input.IsActionPressed("right"))
+            _sprite.FlipH = false;
+        Velocity = velocity;
+    }
+
+    // SIGNALS  --------------------------------------------------------------------------------------------------------
+    private void OnIFramesTimeout()
+    {
+        GD.Print("IFrames ended");
+        damageTaken = false;
+    }
+
+    public void OnDashTimeout()
+    {
+        Dash.isDashing = false;
+        velocity.X = 0;
+        Dash.currentDashCooldown = 0f;
+    }
+}
