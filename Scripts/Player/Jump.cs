37ffc88e9bd704ee90793c691b2adaca4c21898a
Vadim Niedental <vadim.niedental@gmail.com>
2024-12-16 01:04:12 +0100

    Simple Jump method.

---
+++ Scripts/Player/Jump.cs
@@ -0,0 +1,24 @@
+using Godot;
+
+public partial class Jump : Resource
+{
+    private Player Player;
+
+    public void SetPlayer(Player player)
+    {
+        Player = player;
+    }
+
+    
+    public void HandleJump()
+    {
+        if (Player.IsOnFloor())
+        {
+            if (Input.IsActionJustPressed("jump") && Player.IsOnFloor() || Input.IsActionJustPressed("jump") && !Player.IsOnFloor())
+            {
+                // Player._sprite.Play("jump");
+                Player.velocity.Y = Player.jumpForce;
+            }
+        }
+    }
+}
